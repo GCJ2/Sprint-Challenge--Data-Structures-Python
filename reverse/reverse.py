889a7c470b893ebf043909abfc1ef0045fde760c
Greg Johnston <gcjohnston91@gmail.com>
2020-04-24 11:55:09 -0400

    Did reverse with recursion. Oops.

--- reverse/reverse.py
+++ reverse/reverse.py
@@ -47,11 +47,21 @@ class LinkedList:
 
 	def reverse_list(self, node, prev):
 		# You must use recursion for this solution
-		prev = None                             # Set prev to none as it doesn't exist... yet
-		current = self.head                     # Set current node to self.head
-		while current is not None:              # While there is still a node
-			next_node = current.get_next()      # Set the next node to the node after the current node
-			current.set_next(prev)              # Set the node after the current node to the previous node
-			prev = current                      # Set said previous node to the current node
-			current = next_node                 # Set the current node to the next node
-		self.head = prev                        # After loop is done, set the head to prev
+		# [1, 2, 3]
+		if node is not None:                # If there is a node
+			next_node = node.next_node      # Instant var with node to be accessed after current node
+			print(next_node)
+			node.next_node = prev           # Set the next node of current node to previous node
+			prev = node                     # Set prev to the node that was passed in
+			node = next_node                # Update passed in node with next_node
+			self.head = prev                # Set the head to prev
+			self.reverse_list(node, prev)   # Run until node is None
+
+		# prev = None                             # Set prev to none as it doesn't exist... yet
+		# current = self.head                     # Set current node to self.head
+		# while current is not None:              # While there is still a node
+		# 	next_node = current.get_next()      # Set the next node to the node after the current node
+		# 	current.set_next(prev)              # Set the node after the current node to the previous node
+		# 	prev = current                      # Set said previous node to the current node
+		# 	current = next_node                 # Set the current node to the next node
+		# self.head = prev                        # After loop is done, set the head to prev
